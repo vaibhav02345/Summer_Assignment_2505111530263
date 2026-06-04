9a5dfc22f1207f3d3cd3a5e8627dbe070a75aabc
vaibhav02345 <pathakvaibhav913@gmail.com>
2026-06-04 21:20:32 +0530

    Day_2 Assignment uploaded

---
+++ Day_2/Reverse_a_Number.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    int rev = 0;
+
+    while (n > 0) {
+        rev = rev * 10 + n % 10;
+        n /= 10;
+    }
+    cout << "Reversed number = " << rev;
+
+    return 0;
+}
